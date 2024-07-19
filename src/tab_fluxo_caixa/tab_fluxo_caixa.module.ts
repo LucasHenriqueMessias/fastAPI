4f0d70bd893792d131d22926b93418532ff1a204
lucashenriquemessias <lucashenriquemessiasgoncalves@gmail.com>
2024-07-19 12:06:57 -0300

    include columns on tab_fluxo_caixa entity

--- src/tab_fluxo_caixa/tab_fluxo_caixa.module.ts
+++ src/tab_fluxo_caixa/tab_fluxo_caixa.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TabFluxoCaixaService } from './tab_fluxo_caixa.service';
 import { TabFluxoCaixaController } from './tab_fluxo_caixa.controller';
+import { TabFluxoCaixa } from './entities/tab_fluxo_caixa.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TabFluxoCaixa])],
   controllers: [TabFluxoCaixaController],
   providers: [TabFluxoCaixaService],
 })

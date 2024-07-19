73a83a7e12924ca3d64bc91cb6d920bc39f13d31
lucashenriquemessias <lucashenriquemessiasgoncalves@gmail.com>
2024-07-19 17:38:34 -0300

    including methods for tab_fluxo_caixa's API

--- src/tab_fluxo_caixa/tab_fluxo_caixa.service.ts
+++ src/tab_fluxo_caixa/tab_fluxo_caixa.service.ts
@@ -1,26 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTabFluxoCaixaDto } from './dto/create-tab_fluxo_caixa.dto';
 import { UpdateTabFluxoCaixaDto } from './dto/update-tab_fluxo_caixa.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TabFluxoCaixa } from './entities/tab_fluxo_caixa.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class TabFluxoCaixaService {
-  create(createTabFluxoCaixaDto: CreateTabFluxoCaixaDto) {
-    return 'This action adds a new tabFluxoCaixa';
+
+  constructor(
+    @InjectRepository(TabFluxoCaixa)
+    private readonly TabFluxoCaixaRepository:
+    Repository<TabFluxoCaixa>){
+
+    }
+
+  async create(createTabFluxoCaixaDto: CreateTabFluxoCaixaDto) {
+    const fluxo = this.TabFluxoCaixaRepository.create(createTabFluxoCaixaDto)
+    return await this.TabFluxoCaixaRepository.save(fluxo);
   }
 
-  findAll() {
-    return `This action returns all tabFluxoCaixa`;
+  async findAll() {
+    return await this.TabFluxoCaixaRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} tabFluxoCaixa`;
+  async findOne(id: number) {
+    return await this.TabFluxoCaixaRepository.findOne({
+      where: {id}
+    });
   }
 
-  update(id: number, updateTabFluxoCaixaDto: UpdateTabFluxoCaixaDto) {
-    return `This action updates a #${id} tabFluxoCaixa`;
-  }
+ async update(id: number, updateTabFluxoCaixaDto: UpdateTabFluxoCaixaDto) {
+    const fluxo = await this.findOne(id);
+    if (!fluxo){
+      throw new NotFoundException();
+    }  
 
-  remove(id: number) {
-    return `This action removes a #${id} tabFluxoCaixa`;
+    Object.assign(fluxo, updateTabFluxoCaixaDto);
+    return await this.TabFluxoCaixaRepository.save(fluxo);
+}
+
+  async remove(id: number) {
+    const fluxo = await this.findOne(id);
+    if(!fluxo){
+      throw new NotFoundException();
+    }
+    return await this.TabFluxoCaixaRepository.remove(fluxo)
   }
 }

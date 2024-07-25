d2614636e3b2f94ce2bca66cc4b9ef883b0064cb
lucashenriquemessias <lucashenriquemessiasgoncalves@gmail.com>
2024-07-25 15:20:26 -0300

    include controllers to tab_socios

--- src/tab_socios/tab_socios.module.ts
+++ src/tab_socios/tab_socios.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TabSociosService } from './tab_socios.service';
 import { TabSociosController } from './tab_socios.controller';
+import { TabSocio } from './entities/tab_socio.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TabSocio])],
   controllers: [TabSociosController],
   providers: [TabSociosService],
 })
--- src/tab_socios/tab_socios.service.ts
+++ src/tab_socios/tab_socios.service.ts
@@ -1,26 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTabSocioDto } from './dto/create-tab_socio.dto';
 import { UpdateTabSocioDto } from './dto/update-tab_socio.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TabSocio } from './entities/tab_socio.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class TabSociosService {
-  create(createTabSocioDto: CreateTabSocioDto) {
-    return 'This action adds a new tabSocio';
+  constructor(
+    @InjectRepository(TabSocio)
+    private readonly tabSocioRepository:
+    Repository<TabSocio>){
+
+    }
+
+
+  async create(createTabSocioDto: CreateTabSocioDto) {
+    const socio = this.tabSocioRepository.create(createTabSocioDto)
+    return await this.tabSocioRepository.save(socio);
+   }
+
+  async findAll() {
+    return await this.tabSocioRepository.find();
   }
 
-  findAll() {
-    return `This action returns all tabSocios`;
+  async findOne(ID_Socio: number) {
+    return await this.tabSocioRepository.findOne({
+      where: {ID_Socio}
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} tabSocio`;
+  async update(ID_Socio: number, updateTabSocioDto: UpdateTabSocioDto) {
+    
+    const socio = await this.findOne(ID_Socio);
+    if (!socio){
+      throw new NotFoundException();
+    }  
+
+    Object.assign(socio, updateTabSocioDto);
+    return await this.tabSocioRepository.save(socio);  
   }
 
-  update(id: number, updateTabSocioDto: UpdateTabSocioDto) {
-    return `This action updates a #${id} tabSocio`;
-  }
+  
+  async remove(ID_Socio: number) {
+    const socio = await this.findOne(ID_Socio);
+    if (!socio){
+      throw new NotFoundException();
+    }  
 
-  remove(id: number) {
-    return `This action removes a #${id} tabSocio`;
+    return await this.tabSocioRepository.remove(socio);  
   }
 }

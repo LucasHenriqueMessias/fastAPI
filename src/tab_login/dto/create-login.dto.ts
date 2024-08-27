efd53d887ff672d334a2f8a9312be8f11bfd1adb
lucashenriquemessias <lucashenriquemessiasgoncalves@gmail.com>
2024-08-27 09:36:15 -0300

    prettier create-login-dto

--- src/tab_login/dto/create-login.dto.ts
+++ src/tab_login/dto/create-login.dto.ts
@@ -1,12 +1,11 @@
 import { TabLoja } from "src/tab_cliente/entities/tab_loja.entity";
 
 export class CreateLoginDto {
-
     
-user: string;
-salt: string;
-hash: string;
-loja: TabLoja;
-active: boolean;
+    user: string;
+    salt: string;
+    hash: string;
+    loja: TabLoja;
+    active: boolean;
 
 }
